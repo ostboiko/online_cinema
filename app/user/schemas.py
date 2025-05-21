77d769f2f8ca702fb3d15a3f49873a7dec794e1d
Ostap Boiko <138430404+ostboiko@users.noreply.github.com>
2025-05-21 21:38:21 +0300

    adding cart logic and full functionality

---
+++ app/user/schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserRead(BaseModel):
+    id: int
+    email: EmailStr
+    is_active: bool
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
